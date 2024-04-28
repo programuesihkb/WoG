c15c52c1a2d4a67339e438a73e278a65906c8290
MiloJani <emiljano.molla2@gmail.com>
2024-04-28 19:59:27 +0200

    Profile and Bored? in navbar

--- database-include.php
+++ database-include.php
@@ -2,5 +2,5 @@
 define('DBHOST','127.0.0.1');
 define('DBNAME','projektweb');
 define('DBUSER','root');
-define('DBPASS','');
+define('DBPASS','root');
 ?>
---
+++ profile.php
@@ -0,0 +1,10 @@
+<html>
+    <head></head>
+    <body>
+        <?php include "header.php"?>
+
+        <button>Logout</button>
+        <button>Change password</button>
+        <?php include "footer.php"?>
+    </body>
+</html>
