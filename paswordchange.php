ea2726dbdccbbf5abe9c7f199f00ae2c6f660627
NeoImp <franko.ferra19@gmail.com>
2024-04-29 11:18:10 +0200

    ALl of it
    Ndryshim nga chatGPT te pa testuara

--- paswordchange.php
+++ paswordchange.php
@@ -1,21 +1,39 @@
 <?php
-	session_start();
-	include("database-connection.php");
-	if(isset($_POST['Submit']))
-		{
-			$oldpass=md5($_POST['opwd']);
-			$useremail=$_SESSION['login'];
-			$newpassword=md5($_POST['npwd']);
-			$sql=mysqli_query($con,"SELECT password FROM userinfo where password='$oldpass' && email='$useremail'");
-			$num=mysqli_fetch_array($sql);
-			if($num>0)
-				{
-					$con=mysqli_query($con,"update userinfo set password=' $newpassword' where email='$useremail'");
-					$_SESSION['msg1']="Password Changed Successfully !!";
-				}
-			else
-				{
-					$_SESSION['msg1']="Old Password not match !!";
-				}
-		}
+session_start();
+include("database-connection.php");
+
+if(isset($_POST['Submit'])) {
+    $oldpass = md5($_POST['opwd']);
+    $useremail = $_SESSION['login'];
+    $newpassword = md5($_POST['npwd']);
+    
+    $stmt = $con->prepare("SELECT password FROM userinfo WHERE email = ?");
+    $stmt->bind_param("s", $useremail);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if ($row) {
+        if (md5($_POST['opwd']) === $row['password']) {
+            $update_stmt = $con->prepare("UPDATE userinfo SET password = ? WHERE email = ?");
+            $update_stmt->bind_param("ss", $newpassword, $useremail);
+            if ($update_stmt->execute()) {
+                $_SESSION['msg1'] = "Password Changed Successfully !!";
+                header("Location: login_page.html"); // Redirect to login_page.html
+                exit();
+            } else {
+                $_SESSION['msg1'] = "Error updating password.";
+            }
+        } else {
+            $_SESSION['msg1'] = "Old Password does not match !!";
+        }
+    } else {
+        $_SESSION['msg1'] = "User not found.";
+    }
+
+    $stmt->close();
+    $update_stmt->close();
+}
+
+$con->close();
 ?>
